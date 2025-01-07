7fa2f561e051fe51dfb6d1e11f5a1f40ffd45fe7
GaganJangid <121936713+Gaganjangid73@users.noreply.github.com>
2025-01-07 04:40:14 +0000

    contanier update

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,22 @@
+const accountid = 12787
+let accountemail = "gagan@gmail.com"
+var accountpassword ="7483392"
+ accountcity = "jaipur"
+
+let accountstate;
+
+// accountid =784  not allowed bcz it is constant 
+
+accountemail = "yo@gamil.com"
+accountpassword = " 3536323232"
+accountcity = "Delhi"
+
+console.log(accountid);
+console.table([ accountemail, accountpassword, accountcity, accountstate])
+
+
+
+/*
+ prefer not to use var
+ because of issue in block scope 
+*/
---
+++ 01_basics/02_Datatoye.js
@@ -0,0 +1,21 @@
+ "Use Strict" // this is used for treat all JS Code to the newer version/ javascript
+
+//  alert(3+3) we are using node js , not browser 
+
+let name = "gagan"
+let age = 18
+
+// primitative data type 
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof age);
+console.log(typeof null); //. type is object // this is may be asked in interview 
+console.log(typeof undefined); // type is undefied 
+
