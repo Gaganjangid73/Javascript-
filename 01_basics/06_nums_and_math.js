7d47154c786e3e2b29a3f763ecab1272d6c6438a
GaganJangid <121936713+Gaganjangid73@users.noreply.github.com>
2025-01-07 07:12:52 +0000

    conatiner update

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+const score =400 
+
+const newScore = new Number(100)
+// console.log(newScore.toFixed(2)); to fixed is used for the ki decimals me kitne number lene hai fullstop ke baad 
+
+// console.log(Number.MAX_VALUE);  this is used for max value of number
+// console.log(Number.MIN_VALUE); this is used for the min value of number
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));  this is used for the count the hunderds in the indian number.  by default it show the us NUMBER 
+
+
+// ****************************** MATHS *****************************
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));  // this is used for the convert of negative value into the positive value.
+// console.log(Math.round(3.6));// this method is used for to take  the value roundoff.
+// console.log(Math.min(3,8,9,5)); // this is used for to find minimun value in the scope
+// console.log(Math.max(6,6,7,9)); // this is used for to find maximun value in the scope
+
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
